9acd9d5dad72255335f8b7ae9f9c39210701bcfb
amokdad <30440605+amokdad@users.noreply.github.com>
2017-07-26 19:11:04 +0300

    ..

--- app.js
+++ app.js
@@ -262,6 +262,16 @@ var program = {
 
 program.Init();
 
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello %s... Thanks for adding me.", name || 'there');
+        bot.send(reply);
+    }
+});
+
 
 
     
